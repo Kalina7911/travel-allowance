9fd4b2e4d32ea2c0f2ae327c026299217427e13f
Alina <kotnarow@student.wszib.edu.pl>
2023-07-28 12:11:46 +0200

    Initial commit

---
+++ src/main/java/pl/wszib/travelallowance/TravelAllowanceApplication.java
@@ -0,0 +1,13 @@
+package pl.wszib.travelallowance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAllowanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAllowanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wszib/travelallowance/TravelAllowanceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.travelallowance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAllowanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
